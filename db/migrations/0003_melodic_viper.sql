da174aea8a6716507d76396b5a3a456238031884
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-11-22 16:32:29 +0900

    [add]リレーション張ってみたかったのでusersに１対多のタスクを定義してみた

---
+++ db/migrations/0003_melodic_viper.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "tasks" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "tasks_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"title" varchar(255) NOT NULL,
+	"description" varchar(1024),
+	"user_id" integer NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
