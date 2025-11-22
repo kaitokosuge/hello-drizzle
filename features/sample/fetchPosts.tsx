f2f86bf81c8c846bf1b375ad8dd47a345e950aa2
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-11-22 16:35:55 +0900

    [fix]drizzle handleと無関係、不要なスローとハンドリングを削除

--- features/sample/fetchPosts.tsx
+++ features/sample/fetchPosts.tsx
@@ -16,17 +16,11 @@ export async function fetchHello() {
         return { data, error: null };
     } catch (error: unknown) {
         if (error instanceof AxiosError) {
-            console.log('axiosエラーをキャッチしました', error);
+            console.error('axiosエラーをキャッチしました', error);
             return { data: null, error: error.status };
-        } else if (error instanceof TypeError) {
-            console.log('TypeErrorをキャッチしました', error);
-            return {
-                data: null,
-                error: 500,
-            };
         }
-        console.log('予測しないエラーをキャッチしました', error);
+        console.error('予測しないエラーをキャッチしました', error);
 
-        throw { data: null, error: 500 };
+        return { data: null, error: 500 };
     }
 }
